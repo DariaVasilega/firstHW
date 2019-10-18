e1005b7ddf7c537632aeb9a00886da8925fe0659
DariaVasilega <dariavasilega@gmail.com>
2019-10-18 17:01:41 +0300

    Without Move controller

---
+++ app/Move.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App;
+
+use Src\Interfaces\Controller;
+
+class Move implements Controller
+{
+    /**
+     * @param array $params
+     */
+    public function index(array $params = []):void
+    {
+    }
+
+}
